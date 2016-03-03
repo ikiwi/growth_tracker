4dee896b0d8b443aacb7e9cf54c7377bf892b043
ikiwi <kalai.wei@gmail.com>
2016-03-03 15:44:51 -0800

    Angular: Display goals in table format

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -18,13 +18,26 @@
 var app = angular.module('growth_tracker', []);
 
 app.controller('MainCtrl', function ($scope, $http) {
-  var query = "/stories.json";
+  var query_story = "/stories.json";
   $scope.tag = "";
 
-  $http.get(query).then(
+  $http.get(query_story).then(
     function (success) {
       console.log(success);
       $scope.stories = success.data;
+    },
+    function (error) {
+      console.log(error);
+    }
+  );
+
+  var query_goal = "/goals.json";
+  $scope.tag = "";
+
+  $http.get(query_goal).then(
+    function (success) {
+      console.log(success);
+      $scope.goals = success.data;
     },
     function (error) {
       console.log(error);
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,4 @@
 class Goal < ActiveRecord::Base
+  validates :title, :desc, presence: true
   belongs_to :users
 end
